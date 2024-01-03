c73b6796346c248db0b06b8e11b3dcdaaac18194
Bayarsuren.E (Баярсүрэн.Э) <bayarsuren.e@khanbank.com>
2024-01-03 13:13:00 +0800

    alla

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -21,7 +21,40 @@ import ClubExpired from '../pages/Dashboard/Pages/Clubs/expired';
 import TeamsExpired from '../pages/Dashboard/Pages/Teams/expired';
 
 const ROUTES = () => {
+
+    return (
+        <Routes>
+            <Route path='/' element={<Login/>}/>
+            <Route path='/comp-onoolt/:comp/:kg' element={<CompOnooltDetail/>}/>
+            <Route path='/comp-play/:comp/:kg' element={<CompPlay/>}/>
+            <Route path='/comp-jin/:comp/:kg' element={<JinControl/>}/>
+            <Route path="/dashboard" element={<Home />}>
+
+            <Route index element={<Dashboard/>}/>
+                <Route path="teams-req" element={<TeamsReq/>}/>
+                <Route path="teams-active" element={<TeamsActive/>}/>
+                <Route path="teams-expired" element={<TeamsExpired/>}/>
+                <Route path="comp-list" element={<ComptationList/>}/>
+                <Route path="comp-list/:slug" element={<DrawComptation/>}/>
+                <Route path="comp-list/:slug/:org" element={<MeduulegDetail/>}/>
+                <Route path="comp-calendar" element={<ComptationCalendar/>}/>
+                <Route path="club-list/requested" element={<ClubLists/>}/>
+                <Route path="club-list/approved" element={<ClubApproved/>}/>
+                <Route path="club-list/expired" element={<ClubExpired/>}/>
+                <Route path="admins" element={<Admins/>}/>
+            </Route>
+        </Routes>
+    )
+    
     const {currentUser} = useContext(AuthContext);
+    
+    if( !currentUser ){
+        return (
+            <Routes>
+                <Route path='/' element={<Login/>}/>
+            </Routes>
+        )
+    }
 
     if( currentUser === ADMIN.DIRECTOR ){
         return (
